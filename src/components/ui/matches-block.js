ea94840346edd98e188ec5d3b8ab4c9b232c116f
Alex Wong <alexw14@gmail.com>
2019-05-26 22:02:51 -0700

    Add nba team icons and logic in matches block to display team icon

--- src/components/ui/matches-block.js
+++ src/components/ui/matches-block.js
@@ -2,20 +2,31 @@ import React from 'react';
 
 const MatchesBlock = ({ match }) => {
   
+  const getTeamName = (name) => {
+    let arr = name.toLowerCase().split(" ");
+    return arr[arr.length-1];
+  }
+
   let home = '';
   let away = '';
   let homeScore = 0;
   let awayScore = 0;
+  let homeIcon = '';
+  let awayIcon = '';
   if (match.FIELD6 === "@") {
     home = match.Opponent;
     away = 'Los Angeles Lakers';
     homeScore = match.Opp;
     awayScore = match.Tm;
+    homeIcon = getTeamName(match.Opponent);
+    awayIcon = 'lakers';
   } else {
     home = 'Los Angeles Lakers';
     away = match.Opponent;
     homeScore = match.Tm;
     awayScore = match.Opp;
+    homeIcon = 'lakers';
+    awayIcon = getTeamName(match.Opponent);
   }
 
   return (
@@ -26,7 +37,7 @@ const MatchesBlock = ({ match }) => {
       <div className="match_wrapper">
         <div className="match_top">
           <div className="left">
-            <div className="icon"></div>
+            <div className="icon" style={{background:`url(/images/team_icons/${awayIcon}.png)`}}></div>
             <div className="team_name" style={{ fontSize: '28px' }}>{away}</div>
           </div>
           <div className="right" style={{ fontSize: '34px' }}>
@@ -35,7 +46,7 @@ const MatchesBlock = ({ match }) => {
         </div>
         <div className="match_bottom">
           <div className="left">
-            <div className="icon"></div>
+            <div className="icon" style={{background:`url(/images/team_icons/${homeIcon}.png)`}}></div>
             <div className="team_name" style={{ fontSize: '28px' }}>{home}</div>
           </div>
           <div className="right" style={{ fontSize: '34px' }}>
