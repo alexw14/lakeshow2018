e8046961c73e5f1fbe67f43cc273e43504cdcdd4
Alex Wong <alexw14@gmail.com>
2019-05-27 00:03:03 -0700

    adjust height of text and picture in featured section

--- src/components/home/featured/Text.js
+++ src/components/home/featured/Text.js
@@ -6,47 +6,49 @@ import FeaturedPlayer from '../../../resources/images/lakers_featured_player.png
 
 class Text extends Component {
 
-  // animateNumber = () => (
-  //   <Animate
-  //     show={true}
-  //     start={{
-  //       opacity: 0,
-  //       rotate: 0
-  //     }}
-  //     enter={{
-  //       opacity: 1,
-  //       rotate: [360],
-  //       timing: { duration: 1000, ease: easePolyOut }
-  //     }}
-  //   >
-  //     {({ opacity, rotate }) => {
-  //       return (
-  //         <div className="featured_number"
-  //           style={{
-  //             opacity,
-  //             transform: `translate(260px,170px) rotateY(${rotate}deg)`
-  //           }}
-  //         >
-  //           16
-  //         </div>
-  //       )
-  //     }}
-  //   </Animate>
-  // )
-
   animateFirst = () => (
     <Animate
       show={true}
       start={{
         opacity: 0,
         x: 503,
-        y: 450
+        y: 150
       }}
       enter={{
         opacity: [1],
         x: [273],
-        y: [450],
+        y: [150],
         timing: { duration: 500, ease: easePolyOut }
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div className="featured_second"
+            style={{
+              opacity,
+              transform: `translate(${x}px,${y}px)`
+            }}
+          >
+            LA Lakers
+        </div>
+        )
+      }}
+    </Animate>
+  )
+
+  animateSecond = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 300
+      }}
+      enter={{
+        opacity: [1],
+        x: [273],
+        y: [300],
+        timing: { delay: 300, duration: 500, ease: easePolyOut }
       }}
     >
       {({ opacity, x, y }) => {
@@ -64,19 +66,19 @@ class Text extends Component {
     </Animate>
   )
 
-  animateSecond = () => (
+  animateThird = () => (
     <Animate
       show={true}
       start={{
         opacity: 0,
         x: 503,
-        y: 586
+        y: 450
       }}
       enter={{
         opacity: [1],
         x: [273],
-        y: [586],
-        timing: { delay: 300, duration: 500, ease: easePolyOut }
+        y: [450],
+        timing: { delay: 600, duration: 500, ease: easePolyOut }
       }}
     >
       {({ opacity, x, y }) => {
@@ -102,19 +104,19 @@ class Text extends Component {
       }}
       enter={{
         opacity: [1],
-        timing: { delay: 800, duration: 500, ease: easePolyOut }
+        timing: { delay: 250, duration: 500, ease: easePolyOut }
       }}
     >
-      {({ opacity}) => {
+      {({ opacity }) => {
         return (
           <div className="featured_player"
             style={{
               opacity,
-              background:`url(${FeaturedPlayer})`,
-              transform: `translate(650px,201px)`
+              background: `url(${FeaturedPlayer})`,
+              transform: `translate(820px,125px)`
             }}
           >
-      </div>
+          </div>
         )
       }}
     </Animate>
@@ -127,6 +129,7 @@ class Text extends Component {
         {this.animateFirst()}
         {this.animateSecond()}
         {this.animatePlayer()}
+        {this.animateThird()}
       </div>
     );
   }
