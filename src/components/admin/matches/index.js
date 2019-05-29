5f4a6b065d9f979944dd0cda250da1c7623f3078
Alex Wong <alexw14@gmail.com>
2019-05-28 23:30:04 -0700

    add admin games screen

---
+++ src/components/admin/matches/index.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AdminLayout from '../../../hoc/AdminLayout';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { firebaseMatches } from '../../../firebase';
+import { firebaseLooper, reverseArray } from '../../ui/misc';
+
+class AdminMatches extends Component {
+
+  state = {
+    isLoading: true,
+    matches: []
+  }
+
+  componentDidMount() {
+    firebaseMatches.once('value').then((snapshot) => {
+      const matches = firebaseLooper(snapshot);
+      this.setState({
+        isLoading: false,
+        matches: reverseArray(matches)
+      })
+    });
+  }
+
+  render() {
+    return (
+      <AdminLayout>
+        <div>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Opponent</TableCell>
+                  <TableCell>Result</TableCell>
+                  <TableCell>Final</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.matches ?
+                  this.state.matches.map((match, i) => (
+                    <TableRow key={i}>
+                      <TableCell>{match.Date}</TableCell>
+                      <TableCell>
+                        <Link to={`/admin-games/edit-game/${match.id}`}>
+                          {match.FIELD6}{match.Opponent}
+                        </Link>
+                      </TableCell>
+                      <TableCell>{match.FIELD8}</TableCell>
+                      <TableCell>{match.Opp} - {match.Tm} (Lakers)</TableCell>
+                    </TableRow>
+                  ))
+                  : null
+                }
+              </TableBody>
+            </Table>
+          </Paper>
+        </div>
+
+
+
+
+
+        <div className="admin_progress">
+          {this.state.isLoading ?
+            <CircularProgress thickness={7} style={{ color: '#552583' }} />
+            : null
+          }
+        </div>
+      </AdminLayout>
+    );
+  }
+}
+
+export default AdminMatches;
--- src/routes.js
+++ src/routes.js
@@ -9,11 +9,13 @@ import Home from './components/home';
 import SignIn from './components/signin';
 
 import Dashboard from './components/admin/Dashboard';
+import AdminMatches from './components/admin/matches';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoute {...props} exact path="/admin-games" component={AdminMatches} />
         <PrivateRoute {...props} exact path="/dashboard" component={Dashboard} />
         <PublicRoute {...props} exact path="/signin" component={SignIn} restricted={true}/>
         <PublicRoute {...props} exact path="/" component={Home} restricted={false}/>
