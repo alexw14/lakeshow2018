20abef9d8888d4daa0678832538ca9a0f79ac5ac
Alex Wong <alexw14@gmail.com>
2019-06-12 16:30:55 +0800

    update feature player component in home

--- src/components/home/players/Cards.js
+++ src/components/home/players/Cards.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
 import Lebron from '../../../resources/images/players/lebron.png';
+import Lonzo from '../../../resources/images/players/Ball.png';
+import Ingram from '../../../resources/images/players/Ingram.png';
+import Kuzma from '../../../resources/images/players/Kuzma.png';
 import PlayerCard from '../../ui/player-card';
 
 class HomeCards extends Component {
@@ -10,19 +13,35 @@ class HomeCards extends Component {
     cards: [
       {
         bottom: 90,
-        left: 300
+        left: 300,
+        number: "0",
+        firstName: "Kyle",
+        lastName: "Kuzma",
+        bck: Kuzma
       },
       {
         bottom: 60,
-        left: 200
+        left: 200,
+        number: "2",
+        firstName: "Lonzo",
+        lastName: "Ball",
+        bck: Lonzo
       },
       {
         bottom: 30,
-        left: 100
+        left: 100,
+        number: "14",
+        firstName: "Brandon",
+        lastName: "Ingram",
+        bck: Ingram
       },
       {
         bottom: 0,
-        left: 0
+        left: 0,
+        number: "23",
+        firstName: "LeBron",
+        lastName: "James",
+        bck: Lebron
       }
     ]
   }
@@ -52,10 +71,10 @@ class HomeCards extends Component {
               }}
             >
               <PlayerCard
-                number="23"
-                firstName="LeBron"
-                lastName="James"
-                bck={Lebron}
+                number={card.number}
+                firstName={card.firstName}
+                lastName={card.lastName}
+                bck={card.bck}
               />
             </div>
           )
