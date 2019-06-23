990bc6e4ebc7064fd58786d50fef6816ecf61157
Alex Wong <alexw14@gmail.com>
2019-06-22 22:09:07 -0700

    add show games filter

--- src/components/games/index.js
+++ src/components/games/index.js
@@ -12,7 +12,7 @@ class Games extends Component {
     isLoading: true,
     games: [],
     filteredGames: [],
-    playerFilter: 'All',
+    gamesFilter: 'All',
     resultFilter: 'All'
   }
 
@@ -27,13 +27,49 @@ class Games extends Component {
     })
   }
 
+  showGames = (condition) => {
+    const list = this.state.games.filter((game) => {
+      if (condition === "Away") {
+        return game.at === '@'
+      } else {
+        return game.at === ''
+      }
+    });
+    this.setState({
+      filteredGames: condition === "All" ? this.state.games : list,
+      gamesFilter: condition,
+      resultFilter: 'All'
+    })
+  }
+
   render() {
     return (
       <div className="the_matches_container">
         <div className="the_matches_wrapper">
           <div className="left">
             <div className="match_filters">
-
+              <div className="match_filters_box">
+                <div className="tag">
+                  Show Games:
+                </div>
+                <div className="cont">
+                  <div className={`option`}
+                    onClick={() => this.showGames('All')}
+                  >
+                    All
+                  </div>
+                  <div className={`option`}
+                    onClick={() => this.showGames('Home')}
+                  >
+                    Home
+                  </div>
+                  <div className={`option`}
+                    onClick={() => this.showGames('Away')}
+                  >
+                    Away
+                  </div>
+                </div>
+              </div>
             </div>
             <GamesList games={this.state.filteredGames} />
           </div>
