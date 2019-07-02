a4da8f8cb63524c79555a6fd835cb4641d1af79d
Alex Wong <alexw14@gmail.com>
2019-07-01 21:11:09 -0700

    refactor code in to use Fade in home page players section

--- src/components/home/players/Cards.js
+++ src/components/home/players/Cards.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { easePolyOut } from 'd3-ease';
-import Animate from 'react-move/Animate';
+import Fade from 'react-reveal/Fade';
 import Lebron from '../../../resources/images/players/lebron.png';
 import Lonzo from '../../../resources/images/players/Ball.png';
 import Ingram from '../../../resources/images/players/Ingram.png';
@@ -13,40 +12,30 @@ class HomeCards extends Component {
   state = {
     cards: [
       {
-        bottom: 400,
-        left: 0,
         number: "2",
         firstName: "Lonzo",
         lastName: "Ball",
         bck: Lonzo
       },
       {
-        bottom: 400,
-        left: 300,
         number: "14",
         firstName: "Brandon",
         lastName: "Ingram",
         bck: Ingram
       },
       {
-        bottom: 400,
-        left: 600,
         number: "23",
         firstName: "LeBron",
         lastName: "James",
         bck: Lebron
       },
       {
-        bottom: 0,
-        left: 0,
         number: "0",
         firstName: "Kyle",
         lastName: "Kuzma",
         bck: Kuzma
       },
       {
-        bottom: 0,
-        left: 300,
         number: "7",
         firstName: "JaVale",
         lastName: "McGee",
@@ -56,45 +45,25 @@ class HomeCards extends Component {
   }
 
   showAnimateCards = () => (
-    this.state.cards.map((card, i) => (
-      <Animate
-        key={i}
-        show={this.props.show}
-        start={{
-          left: 0,
-          bottom: 0
-        }}
-        enter={{
-          left: [card.left],
-          bottom: [card.bottom],
-          timing: { duration: 5000, ease: easePolyOut }
-        }}
-      >
-        {({ left, bottom }) => {
-          return (
-            <div
-              style={{
-                position: 'absolute',
-                left,
-                bottom
-              }}
-            >
-              <PlayerCard
-                number={card.number}
-                firstName={card.firstName}
-                lastName={card.lastName}
-                bck={card.bck}
-              />
-            </div>
-          )
-        }}
-      </Animate>
-    ))
+    this.state.cards.map((card, i) => {
+      return (
+        <Fade left delay={24} key={i}>
+          <div className="item">
+            <PlayerCard
+              number={card.number}
+              firstName={card.firstName}
+              lastName={card.lastName}
+              bck={card.bck}
+            />
+          </div>
+        </Fade>
+      )
+    })
   )
 
   render() {
     return (
-      <div>
+      <div className="home-card-container">
         {this.showAnimateCards()}
       </div>
     );
